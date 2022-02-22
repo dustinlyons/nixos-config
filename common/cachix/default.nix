097dacefc2ded69fbab8fac8d7eeb16c61467326
Dustin Lyons <hello@dustinlyons.co>
2022-02-22 17:32:32 -0500

    Fix cachix API change

--- common/cachix/default.nix
+++ common/cachix/default.nix
@@ -9,5 +9,5 @@ let
   imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
 in {
   inherit imports;
-  nix.binaryCaches = ["https://cache.nixos.org/"];
+  nix.settings.substituters = ["https://cache.nixos.org/"];
 }
--- common/cachix/nix-community.nix
+++ common/cachix/nix-community.nix
@@ -1,9 +1,9 @@
 {
-  nix = {
-    binaryCaches = [
+  nix.settings = {
+    substituters = [
       "https://nix-community.cachix.org"
     ];
-    binaryCachePublicKeys = [
+    trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
   };
