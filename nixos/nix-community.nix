5573bbe8e35936bd24db7c2468e2564254aab38a
Dustin Lyons <hello@dustinlyons.co>
2022-03-14 16:02:09 -0400

    Move cachix out of common for now

---
+++ nixos/nix-community.nix
@@ -0,0 +1,10 @@
+{
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+}
