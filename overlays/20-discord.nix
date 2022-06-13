9d1d9e5967ab3b10042e97dc26fda2b61b0a5be1
Dustin Lyons <hello@dustinlyons.co>
2022-06-13 16:22:47 -0400

    Remove discord overlay, no longer needed

--- overlays/20-discord.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-  discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball { url = https://discord.com/api/download?platform=linux&format=tar.gz; sha256 = "0hdgif8jpp5pz2c8lxas88ix7mywghdf9c9fn95n0dwf8g1c1xbb"; };
-   }
- );
-}
