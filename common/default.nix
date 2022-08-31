629a9df87c1e9c922dbb96bf931239d62719832e
Dustin Lyons <hello@dustinlyons.co>
2022-08-31 11:54:36 -0400

    Emacs: Use my fork of emacs-overlay to better plan upgrades

--- common/default.nix
+++ common/default.nix
@@ -10,15 +10,15 @@
     };
 
     overlays =
+      # Apply each overlay found in the /overlays directory
       let path = ../overlays; in with builtins;
-      # Load everything in overlays/ dir
       map (n: import (path + ("/" + n)))
           (filter (n: match ".*\\.nix" n != null ||
                       pathExists (path + ("/" + n + "/default.nix")))
                   (attrNames (readDir path)))
-      # We use the nix-community Emacs patches
+
       ++ [(import (builtins.fetchTarball { 
-	       url = "https://github.com/nix-community/emacs-overlay/archive/refs/heads/master.tar.gz";
+	       url = "https://github.com/dustin-lyons/emacs-overlay/archive/refs/heads/master.tar.gz";
                sha256 = "0dzw4azhh96vdg1zl323b4l2ahg3lqkkf8lj22x10s5lg3hy9ryr";
 	}))]; 
   };
