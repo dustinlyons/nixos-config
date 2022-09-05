865ad4573fbf04e2c0498e46652e2cf297173e98
Dustin Lyons <hello@dustinlyons.co>
2022-09-05 12:16:52 -0400

    NixOS: Add polybar and dunst services

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -39,6 +39,56 @@ let
   # Auto mount devices
   services.udiskie.enable = true;
 
+  services.polybar = {
+    enable = true;
+    package = pkgs.polybarFull;
+    script = "polybar top &";
+  };
+
+  services.dunst = {
+    enable = true;
+    package = pkgs.dunst;
+    settings = {
+      global = {
+      monitor = 0;
+      follow = "mouse";
+      border = 0;
+      height = 400;
+      width = 320;
+      offset = "33x65";
+      indicate_hidden = "yes";
+      shrink = "no";
+      separator_height = 0;
+      padding = 32;
+      horizontal_padding = 32;
+      frame_width = 0;
+      sort = "no";
+      idle_threshold = 120;
+      font = "Noto Sans";
+      line_height = 4;
+      markup = "full";
+      format = "<b>%s</b>\n%b";
+      alignment = "left";
+      transparency = 10;
+      show_age_threshold = 60;
+      word_wrap = "yes";
+      ignore_newline = "no";
+      stack_duplicates = false;
+      hide_duplicate_count = "yes";
+      show_indicators = "no";
+      icon_position = "left";
+      icon_theme = "Adwaita-dark";
+      sticky_history = "yes";
+      history_length = 20;
+      browser = "google-chrome-stable";
+      always_run_script = true;
+      title = "Dunst";
+      class = "Dunst";
+      max_icon_size = 64;
+    };
+    };
+  };
+
   programs = common-programs // {};
 
 }
