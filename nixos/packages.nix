b058d4f1ec03238898a931168fa1f1d0c9006340
Dustin Lyons <hello@dustinlyons.co>
2022-09-06 10:05:22 -0400

    Many NixOS packages

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -6,44 +6,80 @@ common-packages ++ [
   _1password-gui
   appimage-run
   betterlockscreen
-  brlaser
-  cawbird
+  brlaser # printer driver
+
+  # Terminal audio visualizer
   cava
+
+  # Apple Music on Linux
   cider
+
+  # Functional testing framework using headless chrome
   cypress
+
   direnv
   discord
   electrum
+
+  # Emoji picker
   emote
+
+  # Manage wallpapers
   feh
+
   firefox
   fontconfig
   font-manager
+
+  # Matrix.org messaging app
   fractal
+
+  # Screenshot tool
   flameshot
+
   gimp
   gnumake
   gnugrep
   google-chrome
+  gtk3
+  glib
   home-manager
+
+  # inotifywait, inotifywatch
+  # For file system events
   inotify-tools
+
   imagemagick
   keepassxc
   libreoffice
   libusb1 # for Xbox controller
   ledger-live-desktop
   openssh
+
+  # Pulse audio controls
   pavucontrol
+
+  # Control media players from command line
+  # Used by other tools
+  playerctl
+
   pinentry-curses
   postgresql
-  playerctl
+
+  # Our file browser
   pcmanfm
+
+  # Context on pkg-config
+  # https://nixos.wiki/wiki/FAQ#I_installed_a_library_but_my_compiler_is_not_finding_it._Why
+  pkg-config
+
   qmk
   rofi
   rnix-lsp # lsp-mode for nix
   spotify
   sqlite
   tree
+  tdesktop # telegram desktop
   unixtools.ifconfig
   unixtools.netstat
   xdg_utils
@@ -51,6 +87,8 @@ common-packages ++ [
   wineWowPackages.stable
   yubikey-manager
   yubikey-manager-qt
-  tdesktop # telegram desktop
   yubikey-agent
+
+  # PDF viewer
+  zathura
 ]
