49dc32175a247aecf8602b449c30bdff488eae15
Dustin Lyons <hello@dustinlyons.co>
2022-10-08 06:26:29 -0400

    NixOS: Add pijulius picom fork to overlay

--- overlays/10-picom.nix
+++ overlays/10-picom.nix
@@ -1,10 +1,10 @@
 final: prev: {
   picom = prev.picom.overrideAttrs (old: {
     src = prev.fetchFromGitHub {
-      owner = "dccsillag";
+      owner = "pijulius";
       repo = "picom";
-      rev = "51b21355696add83f39ccdb8dd82ff5009ba0ae5";
-      sha256 = "crCwRJd859DCIC0pEerpDqdX2j8ZrNAzVaSSB3mTPN8=";
+      rev = "982bb43e5d4116f1a37a0bde01c9bda0b88705b9";
+      sha256 = "YiuLScDV9UfgI1MiYRtjgRkJ0VuA1TExATA2nJSJMhM=";
     };
   });
 }
