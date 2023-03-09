980a49f27fdc4e50cba63fc2b3c052904ca66037
Dustin Lyons <dustin@dlyons.dev>
2023-03-08 20:02:01 -0500

    Emacs: give title bar clean appearance

--- common/config/emacs/init.el
+++ common/config/emacs/init.el
@@ -58,5 +58,10 @@
   :bind
     (("C-c a" . org-agenda)))
 
+(add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist '(ns-appearance . dark)) ;; assuming you are using a dark theme
+(setq ns-use-proxy-icon nil)
+(setq frame-title-format nil)
+
 (require 'org-install)
 (org-babel-load-file "~/.local/share/src/nixos-config/common/config/emacs/config.org")
