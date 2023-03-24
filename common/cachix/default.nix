f452d87085e9c4d8d5c90c4a2dfac65792c674b1
Dustin Lyons <dustin@dlyons.dev>
2023-03-24 14:21:05 -0400

    Clean up cachix related code to be more simple

--- common/cachix/default.nix
+++ common/cachix/default.nix
@@ -1,16 +1,12 @@
 { pkgs, lib, ... }:
-
-let
-  # Import local nix files in directory
-  folder = ./.;
-  toImport = name: value: folder + ("/" + name);
-
-  # Filer for "regular", "directory", "symlink", or "unknown"
-  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key && !(lib.hasPrefix "default" key);
-  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
-in {
-  inherit imports;
-
-  # Set list of caches
-  nix.settings.substituters = ["https://cache.nixos.org/"];
+{
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
