30a832be77e8209b57b46e2e89df0fe3ed6eb682
Dustin Lyons <dustin@dlyons.dev>
2023-08-09 22:42:39 -0400

    Update disko

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -1,5 +1,4 @@
 { ... }: {
-
   disko.devices = {
     disk = {
       nvme0n1 = {
@@ -10,15 +9,23 @@
           partitions = {
             ESP = {
               type = "EF00";  # EFI partition type.
-              size = "100M";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
               };
             };
+            windows = {
+              type = "0700";  # Windows data partition type, for Games
+              size = "900G";  # Adjust as needed.
+              content = {
+                type = "unmanaged";  # Leave this partition unmanaged by Linux.
+              };
+            };
             root = {
-              size = "50%";  # Leaving 50% disk space
+              start = "901G";  # Start immediately after Windows partition.
+              size = "100%";  # Takes the remaining half of the disk space.
               content = {
                 type = "filesystem";
                 format = "ext4";
