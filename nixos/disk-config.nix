e9195e5281b169372a46ef1567754ea8d55cc1ae
Dustin Lyons <dustin@dlyons.dev>
2023-08-09 22:49:49 -0400

    Try disko fix

--- nixos/disk-config.nix
+++ nixos/disk-config.nix
@@ -16,13 +16,6 @@
                 mountpoint = "/boot";
               };
             };
-            windows = {
-              type = "0700";  # Windows data partition type, for Games
-              size = "900G";  # Adjust as needed.
-              content = {
-                type = "unmanaged";  # Leave this partition unmanaged by Linux.
-              };
-            };
             root = {
               start = "901G";  # Start immediately after Windows partition.
               size = "100%";  # Takes the remaining half of the disk space.
