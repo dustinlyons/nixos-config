522679449a08f43bc6b2ac2b2961ee5663729125
Dustin Lyons <dustin@dlyons.dev>
2023-08-10 17:23:46 -0400

    Fix path

--- flake.nix
+++ flake.nix
@@ -107,8 +107,8 @@
 
             echo -e "\033[1;33mCopy keys...\033[0m"
             mkdir -p /mnt/home/${user}/.ssh || { echo -e "\033[1;31mFailed to create .ssh directory!\033[0m"; exit 1; }
-            sudo cp /root/.ssh/id_ed25519_agenix /mnt/home/${user}/id_ed25519 || { echo -e "\033[1;31mFailed to copy private key!\033[0m"; exit 1; }
-            sudo cp /root/.ssh/id_ed25519_agenix.pub /mnt/home/${user}/id_ed25119.pub || { echo -e "\033[1;31mFailed to copy public key!\033[0m"; exit 1; }
+            sudo cp /root/.ssh/id_ed25519_agenix /mnt/home/${user}/.ssh/id_ed25519 || { echo -e "\033[1;31mFailed to copy private key!\033[0m"; exit 1; }
+            sudo cp /root/.ssh/id_ed25519_agenix.pub /mnt/home/${user}/.ssh/id_ed25119.pub || { echo -e "\033[1;31mFailed to copy public key!\033[0m"; exit 1; }
             chmod 644 /mnt/home/${user}/.ssh/id_ed25519.pub || { echo -e "\033[1;31mFailed to set permissions on public key!\033[0m"; exit 1; }
             chmod 600 /mnt/home/${user}/.ssh/id_ed25519 || { echo -e "\033[1;31mFailed to set permissions on private key!\033[0m"; exit 1; }
             chown ${user} /mnt/home/${user}/.ssh/id_ed25519.pub || { echo -e "\033[1;31mFailed to set ownership on private key!\033[0m"; exit 1; }
