a453bbd822b291ea26467cee2c6b2029c2d5eac7
Dustin Lyons <dustin@dlyons.dev>
2023-08-10 16:50:55 -0400

    Try fix

--- flake.nix
+++ flake.nix
@@ -164,7 +164,7 @@
             echo -e "\033[0;32mYubiKey environment set up successfully.\033[0m"
 
             # Setting up SSH directory
-            SSH_DIR=/home/nixos/.ssh
+            SSH_DIR=/root/.ssh
             mkdir -p $SSH_DIR
 
             # Copying the .pub files
