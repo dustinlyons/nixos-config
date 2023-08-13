24e4fda3092f0f87e5d80466ff0acc9ebff73950
Dustin Lyons <dustin@dlyons.dev>
2023-08-13 11:27:46 -0400

    Add syncthing secrets

--- nixos/secrets.nix
+++ nixos/secrets.nix
@@ -7,23 +7,23 @@ let user = "dustin"; in
     "/home/${user}/.ssh/id_ed25519"
   ];
 
-  # age.secrets."syncthing-cert" = {
-  #   symlink = true;
-  #   path = "/home/${user}/.config/syncthing/cert.pem";
-  #   file =  "${secrets}/nixos/syncthing-cert.age";
-  #   mode = "644";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+   age.secrets."syncthing-cert" = {
+     symlink = true;
+     path = "/home/${user}/.config/syncthing/cert.pem";
+     file =  "${secrets}/felix-syncthing-cert.age";
+     mode = "644";
+     owner = "${user}";
+     group = "staff";
+   };
 
-  # age.secrets."syncthing-key" = {
-  #   symlink = true;
-  #   path = "/home/{$user}/.config/syncthing/key.pem";
-  #   file =  "${secrets}/nixos/syncthing-key.age";
-  #   mode = "600";
-  #   owner = "${user}";
-  #   group = "staff";
-  # };
+   age.secrets."syncthing-key" = {
+     symlink = true;
+     path = "/home/{$user}/.config/syncthing/key.pem";
+     file =  "${secrets}/felix-syncthing-key.age";
+     mode = "600";
+     owner = "${user}";
+     group = "staff";
+   };
 
   age.secrets."github-ssh-key" = {
     symlink = false;
