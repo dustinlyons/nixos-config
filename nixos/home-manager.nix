adbcccfb68d45430f87c6d14564a5d8e4dc1b559
Dustin Lyons <dustin@dlyons.dev>
2023-08-14 14:38:45 -0400

    NixOS: autolock settings

--- nixos/home-manager.nix
+++ nixos/home-manager.nix
@@ -59,9 +59,6 @@ in
     enable = true;
     inactiveInterval = 30;
     lockCmd = "${pkgs.betterlockscreen}/bin/betterlockscreen -l dim";
-    xautolock.extraOptions = [
-      "Xautolock.killer: systemctl suspend"
-    ];
   };
 
   # Auto mount devices
