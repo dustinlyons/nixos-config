b428065ee04883fa6b449c6e3eb928ab5a48f910
Dustin Lyons <dustin@dlyons.dev>
2023-09-07 15:00:03 -0400

    MacOS: Remove old emacs-launcher dock command

--- darwin/files.nix
+++ darwin/files.nix
@@ -34,14 +34,6 @@ let
   };
 
   # Script to import Drafts into Emacs org-roam
-  "${xdg_dataHome}/bin/emacs-launcher.command" = {
-    executable = true;
-    text = ''
-      emacsclient -c -n &
-    '';
-  };
-
-  # Script to import Drafts into Emacs org-roam
   "${xdg_dataHome}/bin/import-drafts" = {
     executable = true;
     text = ''
