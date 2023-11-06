7d6252a2538c928fcfcd4af917c873af60245783
Dustin Lyons <dustin@dlyons.dev>
2023-11-06 08:09:11 -0500

    NixOS: enable CUDA support

--- shared/default.nix
+++ shared/default.nix
@@ -8,6 +8,8 @@ in
   nixpkgs = {
     config = {
       allowUnfree = true;
+      cudaSupport = true;
+      cudaCapabilities = ["8.0"];
       allowBroken = true;
       allowInsecure = false;
       allowUnsupportedSystem = true;
