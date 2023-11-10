9946dbf2e41529d79ebf277399a8789908122dca
Dustin Lyons <dustin@dlyons.dev>
2023-11-10 10:23:04 -0500

    Add hostname as option in template

--- templates/starterWithSecrets/nixos/default.nix
+++ templates/starterWithSecrets/nixos/default.nix
@@ -24,7 +24,7 @@ let user = "%USER%";
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.hostName = "felix"; # Define your hostname.
+  networking.hostName = "%HOST%"; # Define your hostname.
   networking.useDHCP = false;
   networking.interfaces.%INTERFACE%.useDHCP = true;
 
