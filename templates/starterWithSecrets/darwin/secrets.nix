4d47ff5e3aadd530ac70ca635001932f8a84c2c5
Dustin Lyons <dustin@dlyons.dev>
2023-11-10 10:00:43 -0500

    Changes to secrets in installWithSecrets template

--- templates/starterWithSecrets/darwin/secrets.nix
+++ templates/starterWithSecrets/darwin/secrets.nix
@@ -7,4 +7,27 @@ let user = "%USER%"; in
   ];
 
   # Your secrets go here
+  #
+  # Note: the installWithSecrets command you ran to boostrap the machine actually copies over
+  #       a Github key. However, if you want to store your  Github keys in your nix-secrets
+  #       repo, you can reference the age files and specify the symlink path. Then just delete
+  #       the bootstrap key.
+  #
+  # age.secrets."github-ssh-key" = {
+  #   symlink = true;
+  #   path = "/Users/${user}/.ssh/id_github";
+  #   file =  "${secrets}/github-ssh-key.age";
+  #   mode = "600";
+  #   owner = "${user}";
+  #   group = "staff";
+  # };
+
+  # age.secrets."github-signing-key" = {
+  #   symlink = false;
+  #   path = "/Users/${user}/.ssh/pgp_github.key";
+  #   file =  "${secrets}/github-signing-key.age";
+  #   mode = "600";
+  #   owner = "${user}";
+  # };
+
 }
