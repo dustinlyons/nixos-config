a3eb1e0f6bf529ba3c04e361e5984b790900796d
Dustin Lyons <dustin@dlyons.dev>
2023-12-11 13:10:41 -0500

    Emacs -> 29.2 bug fixing

--- modules/shared/config/emacs/init.el
+++ modules/shared/config/emacs/init.el
@@ -44,6 +44,7 @@
 ;; -------------------------
 ;; Straight.el Setup
 ;; -------------------------
+(setq straight-repository-branch "develop")
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
