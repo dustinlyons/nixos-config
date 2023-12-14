2ce73eae5192bf3536c977e2f9ffc55a1a84f010
Dustin Lyons <dustin@dlyons.dev>
2023-12-14 11:39:27 -0500

    Fix feather-font overlay

--- overlays/10-feather-font.nix
+++ overlays/10-feather-font.nix
@@ -8,17 +8,15 @@ self: super: with super; {
 
     src = fetchzip {
       url = "https://github.com/dustinlyons/feather-font/archive/refs/tags/${version}.zip";
-      sha256 = "1rdq37bk81k7h7bsdg26djldg0wfgzqhwsqy2y5akp9wh1r76g7b";
-
-      postFetch = ''
-        mkdir -p $out/share/fonts/truetype
-        unzip -D -j $src/feather.ttf -d $out/share/fonts/truetype/
-      '';
+      sha256 = "sha256-Zsz8/qn7XAG6BVp4XdqooEqioFRV7bLH0bQkHZvFbsg=";
     };
+
+    buildInputs = [ unzip ];
+    phases = [ "unpackPhase" "installPhase" ];
 
     installPhase = ''
       mkdir -p $out/share/fonts/truetype
-      cp -p $src/share/fonts/truetype/feather.ttf $out/share/fonts/truetype/
+      cp $src/feather.ttf $out/share/fonts/truetype/
     '';
 
     meta = with lib; {
