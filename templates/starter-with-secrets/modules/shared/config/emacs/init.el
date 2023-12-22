f8d8c5208213e9deb4f5d3ab8012cd52b0aa6744
Dustin Lyons <dustin@dlyons.dev>
2023-12-22 10:32:23 -0500

    templates: shared: Update Emacs config

--- templates/starter-with-secrets/modules/shared/config/emacs/init.el
+++ templates/starter-with-secrets/modules/shared/config/emacs/init.el
@@ -1,7 +1,7 @@
 ;; -------------------------
 ;; Variable Declarations
 ;; -------------------------
-(defvar org-config-file "~/.local/share/src/nixos-config/shared/config/emacs/config.org")
+(defvar org-config-file "~/.local/share/src/nixos-config/modules/shared/config/emacs/config.org")
 (defvar default-config-file "~/.emacs.d/default-config.org")
 (defvar default-config-url "https://raw.githubusercontent.com/dustinlyons/nixos-config/9ad810c818b895c1f67f4daf21bbef31d8b5e8cd/shared/config/emacs/config.org")
 
@@ -13,8 +13,10 @@
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (unless (assoc-default "melpa" package-archives)
+  (message "Warning: MELPA source not found. Adding MELPA to package-archives.")
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
 (unless (assoc-default "org" package-archives)
+  (message "Warning: Org source not found. Adding Org to package-archives.")
   (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
 
 (setq package-enable-at-startup nil)
@@ -24,10 +26,12 @@
 ;; -------------------------
 (unless (package-installed-p 'use-package)
   (package-initialize)
-  (package-install 'use-package))
-(setq use-package-verbose t)
-(setq use-package-always-ensure t)
-(require 'use-package)
+  (if (package-install 'use-package)
+      (message "use-package installed successfully.")
+    (error "Error: Failed to install use-package."))
+  (setq use-package-verbose t)
+  (setq use-package-always-ensure t)
+  (require 'use-package))
 
 ;; -------------------------
 ;; Environment Variables Setup
@@ -36,7 +40,9 @@
   :if (memq window-system '(mac ns x))
   :config
   (setq exec-path-from-shell-variables '("PATH" "GOPATH" "PNPM_HOME"))
-  (exec-path-from-shell-initialize))
+  (if (exec-path-from-shell-initialize)
+      (message "Environment variables initialized successfully.")
+    (error "Error: Failed to initialize environment variables.")))
 
 (when (daemonp)
   (exec-path-from-shell-initialize))
@@ -44,6 +50,7 @@
 ;; -------------------------
 ;; Straight.el Setup
 ;; -------------------------
+(setq straight-repository-branch "develop")
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -55,7 +62,9 @@
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
+  (if (load bootstrap-file nil 'nomessage)
+      (message "Straight.el loaded successfully.")
+    (error "Error: Failed to load Straight.el.")))
 
 (setq straight-use-package-by-default t)
 (package-initialize)
@@ -64,26 +73,34 @@
 ;; Window and UI Setup
 ;; -------------------------
 (defun dl/window-setup ()
-  (column-number-mode)
-  (scroll-bar-mode 0)
-  (menu-bar-mode -1)
-  (tool-bar-mode 0)
-  (winner-mode 1)
-  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
-  (add-to-list 'default-frame-alist '(ns-appearance . dark))
-  (setq ns-use-proxy-icon nil)
-  (setq frame-title-format nil))
+  (condition-case nil
+      (progn
+        (column-number-mode)
+        (scroll-bar-mode 0)
+        (menu-bar-mode -1)
+        (tool-bar-mode 0)
+        (winner-mode 1)
+        (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+        (add-to-list 'default-frame-alist '(ns-appearance . dark))
+        (setq ns-use-proxy-icon nil)
+        (setq frame-title-format nil)
+        (message "Window and UI setup completed successfully."))
+    (error (message "Error occurred in Window and UI setup."))))
 (dl/window-setup)
 
 ;; -------------------------
 ;; Org Mode Setup
 ;; -------------------------
 (defun dl/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (auto-fill-mode 0)
-  (visual-line-mode 1)
-  (setq evil-auto-indent nil))
+  (condition-case nil
+      (progn
+        (org-indent-mode)
+        (variable-pitch-mode 1)
+        (auto-fill-mode 0)
+        (visual-line-mode 1)
+        (setq evil-auto-indent nil)
+        (message "Org mode setup completed successfully."))
+    (error (message "Error occurred in Org mode setup."))))
 
 (use-package org
   :defer t
@@ -93,25 +110,32 @@
         org-ellipsis " ▾"
         org-hide-emphasis-markers t
         org-hide-block-startup nil)
-  :bind
-  (("C-c a" . org-agenda)))
+  :bind (("C-c a" . org-agenda)))
 
 ;; -------------------------
 ;; Default Config Download
 ;; -------------------------
 (defun dl/download-default-config ()
-  (unless (file-exists-p default-config-file)
-    (url-retrieve default-config-url
-                (lambda (_status)
-                ;; delete-region removes the HTTP headers from the downloaded content.
-                (delete-region (point-min) (1+ url-http-end-of-headers))
-                (write-file default-config-file)))))
+  (condition-case nil
+      (progn
+        (unless (file-exists-p default-config-file)
+          (url-retrieve default-config-url
+                        (lambda (_status)
+                          ;; delete-region removes the HTTP headers from the downloaded content.
+                          (delete-region (point-min) (1+ url-http-end-of-headers))
+                          (write-file default-config-file)))
+        (message "Default configuration downloaded successfully."))
+    (error (message "Error occurred while downloading the default configuration.")))))
 
 ;; -------------------------
 ;; Load Org Config or Default
 ;; -------------------------
-(unless (file-exists-p org-config-file)
-  (dl/download-default-config))
-(if (file-exists-p org-config-file)
-    (org-babel-load-file org-config-file)
-  (org-babel-load-file default-config-file))
+(condition-case nil
+    (progn
+      (unless (file-exists-p org-config-file)
+        (dl/download-default-config))
+      (if (file-exists-p org-config-file)
+          (org-babel-load-file org-config-file)
+        (org-babel-load-file default-config-file))
+      (message "Configuration loaded successfully."))
+  (error (message "Error occurred while loading the configuration.")))
