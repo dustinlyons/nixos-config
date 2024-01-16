c847eb6116684db83e771589e8d10f3d34d5440f
Dustin Lyons <dustin@dlyons.dev>
2024-01-16 09:39:06 -0500

    Reduce homebrew casks in templates

--- templates/starter/modules/darwin/casks.nix
+++ templates/starter/modules/darwin/casks.nix
@@ -3,34 +3,24 @@ _:
 [
   # Development Tools
   "homebrew/cask/docker"
-  "insomnia"
-  "ngrok"
-  "postico"
   "visual-studio-code"
 
   # Communication Tools
   "discord"
-  "loom"
   "notion"
   "slack"
   "telegram"
   "zoom"
 
   # Utility Tools
-  "appcleaner"
   "syncthing"
 
   # Entertainment Tools
-  "steam"
   "vlc"
 
   # Productivity Tools
   "raycast"
-  "asana"
 
   # Browsers
   "google-chrome"
-
-  # AI
-  "diffusionbee"
 ]
