6bd4776b6fc900b6772bda04b5be5187506a1ddf
Dustin Lyons <dustin@dlyons.dev>
2024-02-07 07:47:19 -0500

    Add rollback app

--- templates/starter/flake.nix
+++ templates/starter/flake.nix
@@ -65,6 +65,7 @@
         "copy-keys" = mkApp "copy-keys" system;
         "create-keys" = mkApp "create-keys" system;
         "check-keys" = mkApp "check-keys" system;
+        "rollback" = mkApp "rollback" system;
       };
     in
     {
