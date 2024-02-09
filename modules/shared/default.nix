61bb09fc82d2ec48e3a36d31d8989184d497068b
koziserek <koziserek@gmail.com>
2024-02-09 18:14:16 +0100

    Update default.nix
    remm overlays

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -15,17 +15,6 @@ in
       allowUnsupportedSystem = true;
     };
 
-    overlays =
-      # Apply each overlay found in the /overlays directory
-      let path = ../../overlays; in with builtins;
-      map (n: import (path + ("/" + n)))
-          (filter (n: match ".*\\.nix" n != null ||
-                      pathExists (path + ("/" + n + "/default.nix")))
-                  (attrNames (readDir path)))
 
-      ++ [(import (builtins.fetchTarball {
-               url = "https://github.com/dustinlyons/emacs-overlay/archive/refs/heads/master.tar.gz";
-               sha256 = emacsOverlaySha256;
-           }))];
   };
 }
