01abe6004d3a994d9ae1cc8b39f9342eb17f04a9
sonnius <sonnius@users.noreply.github.com>
2024-05-01 11:56:43 +1200

    Remove forgotten lines
    oops

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -288,19 +288,6 @@ let name = "Dustin Lyons";
         ];
       };
     };
-        Host github.com
-          Hostname github.com
-          IdentitiesOnly yes
-      ''
-      (lib.mkIf pkgs.stdenv.hostPlatform.isLinux
-        ''
-          IdentityFile /home/${user}/.ssh/id_github
-        '')
-      (lib.mkIf pkgs.stdenv.hostPlatform.isDarwin
-        ''
-          IdentityFile /Users/${user}/.ssh/id_github
-        '')
-    ];
   };
 
   tmux = {
