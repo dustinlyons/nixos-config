a9b15d2fc0eac29b0d6347a577a4c530c821d6bc
Victor Hooi <victorhooi@yahoo.com>
2024-05-30 15:59:37 +1000

    Add mas note to starter template (with secrets)

--- templates/starter-with-secrets/modules/darwin/home-manager.nix
+++ templates/starter-with-secrets/modules/darwin/home-manager.nix
@@ -34,6 +34,10 @@ in
     # $ nix shell nixpkgs#mas
     # $ mas search <app name>
     #
+    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
+    # you may receive an error message "Redownload Unavailable with This Apple ID".
+    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
+
     masApps = {
       "1password" = 1333542190;
       "wireguard" = 1451685025;
