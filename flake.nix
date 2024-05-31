7911c965929ccb7241e9c6cbc3891312c45a79af
Dustin Lyons <dustin@dlyons.dev>
2024-05-31 09:11:51 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Dustin's Configuration for NixOS and MacOS";
+  description = "General Purpose Configuration for macOS and NixOS";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     agenix.url = "github:ryantm/agenix";
