4c2a48d1b5a80d8961172f0ed53e0dfd2d02ed4b
Dustin Lyons <dustin@dlyons.dev>
2024-08-07 01:09:04 -0400

    Update files.nix

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -63,15 +63,4 @@ in
   ".ssh/pgp_github.pub" = {
     text = githubPublicSigningKey;
   };
-
-  # Used in Emacs config.org to load projects and tasks in org-agenda
-  ".emacs.d/agenda.txt" = {
-    text = ''
-      ~/.local/share/org-roam/20220419121404-todo.org
-      ~/.local/share/org-roam/20230712154159-health.org
-      ~/.local/share/org-roam/20230712154441-family_social.org
-      ~/.local/share/org-roam/20230712154303-business.org
-      ~/.local/share/org-roam/20210919225144-home_lab.org
-    '';
-  };
 }
