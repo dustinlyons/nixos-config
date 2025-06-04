0fde3b2a96c1f20972941673a5b7a301adb68e78
lessuseless <179788364+lessuselesss@users.noreply.github.com>
2025-06-04 15:17:22 -0600

    Update home-manager.nix

--- templates/starter/modules/shared/home-manager.nix
+++ templates/starter/modules/shared/home-manager.nix
@@ -21,7 +21,7 @@ let name = "%NAME%";
       }
     ];
 
-    initExtraFirst = ''
+    initContent = ''
       if [[ -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]]; then
         . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
         . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
