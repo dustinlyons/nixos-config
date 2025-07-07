bab0c48bc0979458813cfb3abd0dd6fec21013f3
Dustin Lyons <dustin@dlyons.dev>
2025-07-07 14:21:27 -0400

    Remove unused scripts

--- modules/nixos/files.nix
+++ modules/nixos/files.nix
@@ -12,49 +12,6 @@ let
     '';
   };
 
-  "${xdg_dataHome}/bin/movesinks" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      pacmd set-default-sink $1
-      pacmd list-sink-inputs | grep index | while read line
-      do
-        pacmd move-sink-input `echo $line | cut -f2 -d' '` $1
-      done
-    '';
-  };
-
-  "${xdg_dataHome}/bin/speakers" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      # Script to change audio format to headphones and check if the sink exists
-
-      # Define the sink name
-      SINK_NAME="alsa_output.usb-Audioengine_Audioengine_2_-00.analog-stereo"
-
-      # Check if the sink exists
-      if pactl list short sinks | grep -q "$SINK_NAME"; then
-        # Sink exists, set it as the default
-        pacmd set-default-sink "$SINK_NAME"
-        movesinks "$SINK_NAME"
-      else
-        # Sink does not exist, print message
-        echo "Turn on your speakers, stupid."
-      fi
-    '';
-  };
-
-  "${xdg_dataHome}/bin/headphones" = {
-    executable = true;
-    text = ''
-      #!/usr/bin/env bash
-      # Changes audio format to headphones
-      pacmd set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
-      movesinks alsa_output.pci-0000_00_1f.3.analog-stereo
-    '';
-  };
-
   "${xdg_configHome}/swappy/config" = {
     text = ''
       [Default]
