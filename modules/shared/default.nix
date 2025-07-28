3c69aedeffc020c0a3221555ea8a80ad58c188a4
Dustin Lyons <dustin@dlyons.dev>
2025-07-28 00:37:06 -0400

    Update flake, emacs overlay

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  emacsOverlaySha256 = "0rvpskk1z996arcj48ch4g08lvid6r856m0x5376y7lfjxlsawga";
+  emacsOverlaySha256 = "17cg7xqdlnq169cl3blkdjhw7pzjp40v5shd0ck27vgb09mahy05";
 in
 {
 
