7148b3dea6203c7310a681852698884fec192ae5
Dustin Lyons <dustin@dlyons.dev>
2025-08-27 12:53:46 -0400

    Fix statix lint

--- hosts/nixos/garfield/hardware-configuration.nix
+++ hosts/nixos/garfield/hardware-configuration.nix
@@ -8,10 +8,12 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "usbhid" "sd_mod" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+  };
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/e194a154-b519-45c6-a875-efdd1704bdfe";
