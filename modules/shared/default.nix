381e16d8d1f8dcab97f1ebc001739bfdb08a3f75
Dustin Lyons <dustin@dlyons.dev>
2026-05-29 15:34:00 -0400

    Update emacs-overlay

--- modules/shared/default.nix
+++ modules/shared/default.nix
@@ -1,7 +1,7 @@
 { config, pkgs, claude-code, ... }:
 
 let
-  emacsOverlaySha256 = "1ayppwhwify85sx4j7mljxhc3pmsi3mgzfps05a0gglmx4jik2xc";
+  emacsOverlaySha256 = "17cigsz0nbc826vdziywanc30vr795ihasmrrpksr6y7d3gm4f6w";
   
   # Shared Emacs package configuration
   myEmacs = import ./emacs.nix { inherit pkgs; };
